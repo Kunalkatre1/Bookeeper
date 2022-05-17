142f6ff2bf2326b03a5050ddc40ba42d81fcd483
Kunal Katre <katre.kunal25@gmail.com>
2022-05-18 00:07:06 +0530

    Added Modal with form validations

---
+++ script.js
@@ -0,0 +1,61 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// Show Modal, Focus on Input
+
+function showModal(){
+
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+
+// Validate Form
+
+function validateForm(nameValue, urlValue){
+
+    const expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/g;
+    const regex = new RegExp(expression);
+
+    if (!nameValue || !urlValue){
+        alert('Please submit values for both feilds!');
+    }
+
+    if(!urlValue.match(regex)){
+        alert('Please provide a valid web address');
+        return false;
+    }
+
+    //Valid
+    return true;
+}
+
+
+// Handle Data from Form
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+
+    if (!urlValue.includes('http://') && !urlValue.includes('https://')){
+        urlValue = `https://${urlValue}`;
+    }
+
+    if (!validateForm(nameValue, urlValue)){
+        return false;
+    }
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
